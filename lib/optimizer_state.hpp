a0bee1fd68f8be9cd337f93ba22108afa040533f
kennyzzhang <kennyzzhang@gmail.com>
2023-07-07 15:31:03 -0400

    Untested score calculation

--- lib/optimizer_state.hpp
+++ lib/optimizer_state.hpp
@@ -33,7 +33,7 @@ class OptimizerState {
 public:
   struct grid_init_tag {};
   OptimizerState(grid_init_tag, const Problem& problem);
-  double calc_cost();
+  uint64_t calc_cost();
 };
 
 OptimizerState::OptimizerState(grid_init_tag, const Problem& problem)
@@ -61,30 +61,62 @@ OptimizerState::OptimizerState(grid_init_tag, const Problem& problem)
   }
 }
 
-double OptimizerState::calc_cost() {
-  std::vector<Curve_2> curves;
+uint64_t OptimizerState::calc_cost() {
+  uint64_t score = 0;
   for (const auto& attendee : problem.attendees) {
-    const auto& a = attendee.location;
-    curves.clear();
+    std::vector<Curve_2> curves;
+    const auto& raw_a = attendee.location;
+    Point_2 a{raw_a.x, raw_a.y};
     for (int i = 0; i < problem.musicians.size(); ++i) {
-      const auto& b = placements[i];
-      curves.emplace_back(Point_2{a.x, a.y}, Point_2{b.x, b.y});
-      curves.emplace_back(Point_2{b.x, b.y}, 5);
+      const auto& raw_b = placements[i];
+      Point_2 b{raw_b.x, raw_b.y};
+      curves.push_back({a, b});
+      curves.emplace_back(b, 5);
     }
-    std::vector<Res_Point_2> pts;
+    std::vector<Res_Point_2> raw_pts;
     CGAL::compute_intersection_points(curves.begin(), curves.end(),
-        std::back_inserter(pts));
-    std::sort(pts.begin(), pts.end(), [&](const auto& rpt1, const auto& rpt2){
-        SqrtPoint_2 pt1{rpt1.x(), rpt1.y()};
-        SqrtPoint_2 pt2{rpt2.x(), rpt2.y()};
-        return SqrtDirection_2{SqrtSegment_2{a, pt1}} < SqrtDirection_2{SqrtSegment_2{a, pt2}};
-        });
-    std::vector<int> placement_idx(problem.musicians.size());
-    std::iota(placement_idx.begin(), placement_idx.end(), 0);
-    std::sort(placement_idx.begin(), placement_idx.end(), [&](const auto& idx1, const auto& idx2){
-        return Direction_2{Segment_2{a, placement[idx1]}} < Direction_2{Segment_2{a, placement[idx2]}};
-        });
+        std::back_inserter(raw_pts));
+
+    std::vector<std::tuple<SqrtDirection_2, SqrtKernel::FT, int>> vision_pts;
+
+    SqrtPoint_2 a_sqrt = CGAL::Cartesian_converter<Kernel, SqrtKernel>()(a);
+
+    for (int i = 0; i < problem.musicians.size(); ++i) {
+      const auto& raw_b = placements[i];
+      Point_2 b{raw_b.x, raw_b.y};
+      Curve_2 vision_curves[] = {{a, b}, {b, 5}};
+      std::array<Res_Point_2, 1> raw_pt;
+      CGAL::compute_intersection_points(std::begin(vision_curves), std::end(vision_curves), raw_pt.begin());
+      SqrtPoint_2 pt = {raw_pt[0].x(), raw_pt[0].y()};
+      vision_pts.push_back({SqrtDirection_2{SqrtSegment_2{a_sqrt, pt}}, CGAL::squared_distance(a_sqrt, pt), i});
+    }
+
+    for (const auto& raw_pt : raw_pts) {
+      SqrtPoint_2 pt{raw_pt.x(), raw_pt.y()};
+      if (pt == a_sqrt) continue;
+      vision_pts.push_back({SqrtDirection_2{SqrtSegment_2{a_sqrt, pt}}, CGAL::squared_distance(a_sqrt, pt), problem.musicians.size()});
+    }
+    std::sort(vision_pts.begin(), vision_pts.end());
+
+    bool visible = true;
+    for (int i = 0; i < vision_pts.size(); ++i) {
+      const auto& [dir, distsq, idx] = vision_pts[i];
+      if (i > 0) {
+        const auto& [pdir, pdistsq, pidx] = vision_pts[i - 1];
+        if (dir > pdir) {
+          visible = true;
+        }
+      }
+      if (idx >= problem.musicians.size()) {
+        visible = false;
+      } else if (visible) {
+        const auto& raw_b = placements[idx];
+        Point_2 b{raw_b.x, raw_b.y};
+        score += std::ceil(1000000 * attendee.tastes[problem.musicians[idx]] / CGAL::to_double(CGAL::squared_distance(a, b)));
+      }
+    }
   }
+  return score;
 }
 
 #endif // INCLUDE_OPTIMIZER_STATE_HPP
