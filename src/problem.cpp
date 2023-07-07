f97fa94fff4a794f55300399349926666338feac
Jacky Xie <jackyxie2520@outlook.com>
2023-07-07 13:48:55 -0400

    SFML visualization

--- src/problem.cpp
+++ src/problem.cpp
@@ -3,6 +3,8 @@
 #include <string>
 #include "../lib/problem.hpp"
 
+#include <SFML/Graphics.hpp>
+
 void parse_problem(const json& data, Problem& problem) {
     data.at("room_width").get_to(problem.room.x); 
     data.at("room_height").get_to(problem.room.y); 
@@ -52,6 +54,44 @@ void print_problem(const Problem& problem) {
     std::cout << "\n]\n"; 
 }
 
+void draw_problem(Problem &problem) {
+    constexpr double width = 1000, height = 1000;
+    sf::RenderWindow window(sf::VideoMode(width, height), "Problem");
+    
+    double scale = fmin(width / problem.room.x, height / problem.room.y);
+
+    sf::RectangleShape room(sf::Vector2f(problem.room.x * scale, problem.room.y * scale));
+    room.setFillColor(sf::Color::White);
+    window.draw(room);
+
+    sf::RectangleShape stage(sf::Vector2f(problem.stage.x * scale, problem.stage.y * scale));
+    stage.setPosition(problem.stage_bottom_left.x * scale, problem.stage_bottom_left.y * scale);
+    stage.setFillColor(sf::Color(216, 64, 64));
+    window.draw(stage);
+
+    for (auto &attendee : problem.attendees) {
+        sf::CircleShape shape(2.f);
+        shape.setFillColor(sf::Color(128, 128, 128));
+        shape.setOrigin(shape.getRadius(), shape.getRadius()); // center of circle
+        shape.setPosition(attendee.location.x * scale, attendee.location.y * scale);
+        window.draw(shape);
+    }
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        /*
+        window.clear();
+        window.draw(shape);
+        */
+        window.display();
+    }
+}
+
 int main(int argc, char** argv) {
     std::ifstream f (argv[1]); 
     auto data_str = json::parse(f)["Success"].template get<std::string>(); 
@@ -59,4 +99,6 @@ int main(int argc, char** argv) {
     Problem problem; 
     parse_problem(data, problem); 
     print_problem(problem); 
+    
+    draw_problem(problem);
 }
